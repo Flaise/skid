c7c3058846a09fe1957240b7e2dc257d23895427
Flaise <Flaise.Saffron@gmail.com>
2014-05-18 15:49:39 -0500

    eventdispatcher.js in TypeScript

--- eventdispatcher.js
+++ eventdispatcher.js
@@ -1,71 +1,68 @@
-'use strict'
-
-if(typeof require !== 'undefined') {
-    var LinkedList = require('./linkedlist')
-    var bind_until = require('./index').bind_until
+'use strict';
+if (typeof require !== 'undefined') {
+    var LinkedList = require('./linkedlist');
+    var bind_until = require('./index').bind_until;
 }
 
+var EventDispatcher = (function () {
+    function EventDispatcher() {
+        this.callbacks = new LinkedList();
+    }
+    EventDispatcher.prototype.listen = function (callback) {
+        if (!callback.apply)
+            throw new Error();
 
-function EventDispatcher() {
-    this.callbacks = new LinkedList()
-}
-EventDispatcher.prototype = {
-    listen: function(callback) {
-        if(!callback.apply) throw new Error()
-        var node = this.callbacks.addFirst(callback) // it might break some behaviors to call listeners in reverse order, but it allows insertion during iteration
-        // if it doesn't work, another test will be added
-        return bind_until(node.remove.bind(node))
-    },
-    listenOnce: function(callback) {
-        if(!callback.apply) throw new Error()
-        var remove = this.listen(function listenOnceWrapper(__varargs__) {
-            remove() // remove will be initialized by the time this is called because there is no event for 'listener-added'
-            callback.apply(null, arguments)
-        })
-        return remove
-    },
-    proc: function(__varargs__) {
-        var args = arguments // make visible to foreach body
-        this.callbacks.forEach(function eachCallback(callback) {
-            callback.apply(null, args)
-        })
-    },
-    onlyWhen: function(reactant) {
-        var result = new EventDispatcher()
+        // it might break some behaviors to call listeners in reverse order, but it allows insertion
+        // during iteration
+        var node = this.callbacks.addFirst(callback);
+
+        return bind_until(function () {
+            return node.remove();
+        });
+    };
+    EventDispatcher.prototype.listenOnce = function (callback) {
+        if (!callback.apply)
+            throw new Error();
+
+        var remove = this.listen(function (__varargs__) {
+            // remove will be initialized by the time this closure is called because there is no
+            // event for 'listener-added'
+            remove();
+            callback.apply(null, arguments);
+        });
+        return remove;
+    };
+    EventDispatcher.prototype.proc = function (__varargs__) {
+        var args = arguments;
+        this.callbacks.forEach(function (callback) {
+            return callback.apply(null, args);
+        });
+    };
+    EventDispatcher.prototype.onlyWhen = function (reactant) {
+        var result = new EventDispatcher();
 
         ////////////////////////////////////////////////////// TODO: This listener needs to be removed when result has none of its own listeners, and re-added when it does
-        var remove = this.listen(function(__varargs__) {
-            if(reactant.value)
-                result.proc.apply(result, arguments)
-        })
+        var remove = this.listen(function (__varargs__) {
+            if (reactant.value)
+                result.proc.apply(result, arguments);
+        });
 
-        return result
-    },
-    aggregate: function(other) {
-        var result = new EventDispatcher()
+        return result;
+    };
+    EventDispatcher.prototype.aggregate = function (other) {
+        var result = new EventDispatcher();
 
         ////////////////////////////////////////////////////// TODO: These listeners need to be removed when result has none of its own listeners, and re-added when it does
-        var removeA = this.listen(result.proc.bind(result))
-        var removeB = other.listen(result.proc.bind(result))
+        // using bind() to retain argument list
+        var removeA = this.listen(result.proc.bind(result));
+        var removeB = other.listen(result.proc.bind(result));
 
-        return result
-    }
+        return result;
+    };
+    return EventDispatcher;
+})();
+
+if (typeof module !== 'undefined') {
+    module.exports = EventDispatcher;
 }
-
-// `predicate` should generally have no side effects.
-/*EventDispatcher.prototype.filterEvents = function(predicate) {
-	var result = new EventDispatcher()
-	
-	////////////////////////////////////////////////////// TODO: This listener needs to be removed when result has none of its own listeners, and re-added when it does
-	var remove = this.listen(function(__varargs__) {
-		if(predicate.apply(null, arguments))
-			result.proc.apply(result, arguments)
-	})
-	
-	return result
-}*/
-
-
-if(typeof module !== 'undefined') {
-    module.exports = EventDispatcher
-}
+//# sourceMappingURL=eventdispatcher.js.map
---
+++ eventdispatcher.ts
@@ -0,0 +1,67 @@
+'use strict'
+
+declare function require(name:string)
+declare var module:any
+
+if(typeof require !== 'undefined') {
+    var LinkedList = require('./linkedlist')
+    var bind_until = require('./index').bind_until
+}
+
+class EventDispatcher {
+    callbacks = new LinkedList()
+    
+    constructor() {
+    }
+    listen(callback) {
+        if(!callback.apply)
+            throw new Error()
+        
+        // it might break some behaviors to call listeners in reverse order, but it allows insertion
+        // during iteration
+        var node = this.callbacks.addFirst(callback)
+        
+        return bind_until(() => node.remove())
+    }
+    listenOnce(callback) {
+        if(!callback.apply)
+            throw new Error()
+            
+        var remove = this.listen(__varargs__ => {
+            // remove will be initialized by the time this closure is called because there is no
+            // event for 'listener-added'
+            remove()
+            callback.apply(null, arguments)
+        })
+        return remove
+    }
+    proc(__varargs__?) {
+        var args = arguments // make visible to foreach body
+        this.callbacks.forEach(callback => callback.apply(null, args))
+    }
+    onlyWhen(reactant) {
+        var result = new EventDispatcher()
+
+        ////////////////////////////////////////////////////// TODO: This listener needs to be removed when result has none of its own listeners, and re-added when it does
+        var remove = this.listen(__varargs__ => {
+            if(reactant.value)
+                result.proc.apply(result, arguments)
+        })
+
+        return result
+    }
+    aggregate(other) {
+        var result = new EventDispatcher()
+
+        ////////////////////////////////////////////////////// TODO: These listeners need to be removed when result has none of its own listeners, and re-added when it does
+        // using bind() to retain argument list
+        var removeA = this.listen(result.proc.bind(result))
+        var removeB = other.listen(result.proc.bind(result))
+
+        return result
+    }
+}
+
+if(typeof module !== 'undefined') {
+    module.exports = EventDispatcher
+}

aad005e3d0add85d196b9883ac24e5a7612c5b78
Flaise <x.Flaise@gmail.com>
2017-06-15 03:32:56 -0500

    Pie slices render with closed paths.

--- src/scene/pie-paths.js
+++ src/scene/pie-paths.js
@@ -7,40 +7,42 @@ export default class PiePaths extends Avatar {
         this._paths = []
         this._interpolands = avatars.interpolands
     }
-    
+
     draw(context) {
         for(var i = 0; i < this._paths.length; i += 1) {
             const path = this._paths[i]
             if(!path.scale.curr)
                 continue
-            
+
             let startAngle = path.startAngle - .25
             let endAngle = startAngle + path.breadth
             startAngle = toRadians(startAngle)
             endAngle = toRadians(endAngle)
-            
+
             context.moveTo(0, 0)
+            context.beginPath()
             context.arc(0, 0, path.scale.curr / 2 * path.innerRadius, endAngle, startAngle,
                         path.breadth >= 0)
             context.arc(0, 0, path.scale.curr / 2, startAngle, endAngle, path.breadth < 0)
+            context.closePath()
         }
     }
-    
+
     make(breadth, startAngle, innerRadius, scale) {
         const result = {
             // Distance of second jaw from first jaw. Positive is clockwise.
             breadth: breadth,
-            
+
             // Distance of first jaw from north. Positive is clockwise.
             startAngle: startAngle,
-            
+
             innerRadius: innerRadius,
             scale: this._interpolands.make(scale || 1)
         }
         this._paths.push(result)
         return result
     }
-    
+
     subremove() {
         for(var i = 0; i < this._paths.length; i += 1)
             this._paths[i].scale.remove()

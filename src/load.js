7de5bde311f46a48bed1ac767de5e2faa791b71f
Flaise <x.Flaise@gmail.com>
2018-02-20 21:51:51 -0600

    Added function for loading generic data.

--- src/load.js
+++ src/load.js
@@ -2,7 +2,7 @@ const {Icon} = require('./scene/icon');
 const {handle} = require('./event');
 
 export function load(state) {
-    state.load = {requests: 0, done: false};
+    state.load = {requests: 0, completions: 0, done: false, loaders: {}};
 
     handle(state, 'load');
     if (state.load.requests === 0) {
@@ -11,43 +11,55 @@ export function load(state) {
     }
 }
 
-export function startLoading(state) {
+export function startLoading(state, size) {
+    if (state.load.done) throw new Error("load assets during the 'load' event");
     state.load.requests += 1;
+    const id = state.load.requests;
+    if (size) progressLoading(state, id, 0, size);
+    return id;
 }
 
-export function doneLoading(state) {
-    state.load.requests -= 1;
-    handle(state, 'load_progress'); // TODO: needs parameter with amount of progress
-    if (!state.load.done && state.load.requests === 0) {
+export function progressLoading(state, id, loaded, total) {
+    if (state.load.done) throw new Error("load assets during the 'load' event");
+    if (isNaN(loaded) || isNaN(total)) {
+        loaded = 0;
+        total = 0;
+    }
+    if (!state.load.loaders[id]) state.load.loaders[id] = {};
+    state.load.loaders[id].loaded = loaded;
+    state.load.loaders[id].total = total;
+
+    if (Object.keys(state.load.loaders).length < state.load.requests) return;
+
+    let allLoaded = 0;
+    let allTotal = 0;
+    for (const loader of Object.values(state.load.loaders)) {
+        allLoaded += loader.loaded;
+        allTotal += loader.total;
+    }
+    handle(state, 'load_progress', allLoaded / allTotal);
+}
+
+export function doneLoading(state, id) {
+    if (state.load.done) throw new Error("load assets during the 'load' event");
+    state.load.completions += 1;
+    if (state.load.completions === state.load.requests) {
         state.load.done = true;
         handle(state, 'load_done');
     }
 }
 
-function loadImage(state, source, onload) {
-    startLoading(state);
-    const image = new window.Image();
-    image.onload = () => {
-        image.onload = image.onerror = undefined;
-        doneLoading(state);
-        if (onload) onload(image);
-    };
-    image.onerror = () => {
-        console.error(`Unable to load image ${source}`);
-        image.onload = image.onerror = undefined;
-        doneLoading(state);
-    };
-    image.src = source;
-    return image;
-}
-
-export function loadIcon(state, source, ax, ay, diameter) {
+export function loadIcon(state, source, ax, ay, diameter, sizeBytes) {
     if (typeof source === 'string') {
         const icon = new Icon();
-        loadImage(state, source, (image) => {
-            icon.image = image;
+        const image = new window.Image();
+        icon.image = image;
+        image.onload = () => {
             icon.layout = computeLayout(ax, ay, diameter, image.width, image.height, 0, 0,
                                         image.width, image.height, true);
+        };
+        loadData(state, source, sizeBytes).then((source) => {
+            image.src = source;
         });
         return icon;
     } else {
@@ -118,4 +130,41 @@ function computeLayout(ax, ay, diameter, untrimmedWidth, untrimmedHeight,
     }
 
     return layout
+}
+
+export function loadData(state, imageUrl, total) {
+    const id = startLoading(state, total);
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', imageUrl, true);
+        xhr.responseType = 'arraybuffer';
+
+        xhr.onprogress = (event) => {
+            if (event.lengthComputable) {
+                progressLoading(state, id, event.loaded, event.total);
+            }
+        };
+
+        xhr.onloadend = () => {
+            if (!xhr.status.toString().match(/^2/)) {
+                reject(xhr);
+                return;
+            }
+
+            const options = {};
+            const headers = xhr.getAllResponseHeaders();
+            const match = headers.match(/^Content-Type\:\s*(.*?)$/mi);
+
+            if (match && match[1]) {
+                options.type = match[1];
+            }
+
+            const blob = new Blob([xhr.response], options);
+            progressLoading(state, id, blob.size, blob.size);
+            resolve(window.URL.createObjectURL(blob));
+            doneLoading(state, id);
+        }
+
+        xhr.send();
+    });
 }

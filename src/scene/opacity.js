832ebe9574c3a93a4df8fad0b4f37a75f62aaeed
Flaise <x.Flaise@gmail.com>
2020-04-23 10:49:15 -0500

    Limiting opacity to 0-1 alpha values

--- src/scene/opacity.js
+++ src/scene/opacity.js
@@ -14,7 +14,7 @@ export class Opacity extends Group {
 
         const prev = context.globalAlpha
         if(prev !== this.alpha.curr)
-            context.globalAlpha = this.alpha.curr
+            context.globalAlpha = Math.max(1, Math.min(0, this.alpha.curr))
         super.draw(context)
         if(prev !== this.alpha.curr)
             context.globalAlpha = prev

911698a9d9e533e84415859c7218743f9f1929f0
Flaise <x.Flaise@gmail.com>
2023-10-16 14:47:46 -0700

    Added FillAvatar for filling screen with a color and setting image smoothing.

---
+++ src/scene/fillavatar.js
@@ -0,0 +1,26 @@
+import {Avatar} from './avatar'
+
+export class FillAvatar extends Avatar {
+    constructor(container, fillStyle, imageSmoothingEnabled) {
+        super(container);
+        this.fillStyle = fillStyle;
+
+        // Affects smoothing for the entire rendering context.
+        // Here because filling and the smoothing setting both often happen at the beginning of
+        // rendering.
+        this.imageSmoothingEnabled = imageSmoothingEnabled;
+    }
+
+    draw(context) {
+        if (this.imageSmoothingEnabled !== undefined) {
+            context.imageSmoothingEnabled = this.imageSmoothingEnabled;
+            context.mozImageSmoothingEnabled = this.imageSmoothingEnabled;
+        }
+
+        if (this.fillStyle) {
+            const canvas = context.canvas;
+            context.fillStyle = this.fillStyle;
+            context.fillRect(0, 0, canvas.width, canvas.height);
+        }
+    }
+}

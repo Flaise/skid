dd29bd05a2d14adbdfb13fe2f0a3110865a40b88
Flaise <x.Flaise@gmail.com>
2023-10-22 11:31:03 -0700

    Revised audio.js to use new loading API.

--- src/audio.js
+++ src/audio.js
@@ -1,48 +1,56 @@
 import { extname } from 'path';
 import { Howl, Howler } from 'howler';
-import { loadData, startLoading, doneLoading, errorLoading } from './load';
+import { loadData, errorLoading, finalizeLoadingPromise } from './load';
 import { addHandler, handle } from './event';
+import { isArray } from './is';
 
-function load(state, eventCode, howlArgs, id) {
+function load(state, eventCode, howlArgs) {
     const sound = new Howl(howlArgs);
-    sound.once('loaderror', (id, error) => {
-        console.error('sound load error ' + error);
-        if (!state.load) {
-            // NOTE: not yet sure what causes this
-            return;
-        }
-        errorLoading(state);
-    });
-    sound.once('load', () => {
-        handle(state, `${eventCode}_load_done`, sound);
-        doneLoading(state, id);
-    });
     addHandler(eventCode, () => {
         sound.play();
     });
     addHandler(`${eventCode}_stop`, () => {
         sound.stop();
     });
+
+    return new Promise((resolve, reject) => {
+        sound.once('loaderror', (_id, error) => {
+            reject(error);
+        });
+        sound.once('load', () => {
+            handle(state, `${eventCode}_load_done`, sound);
+            resolve();
+        });
+    });
 }
 
+// Basic usage: loadAudio(state, 'click', { src: 'click.wav' });
+//
+// Plays 'click.wav' each time the 'click' event is triggered.
+//
+// To support multiple formats, pass an array of paths:
+//
+// loadAudio(state, 'click', { src: ['click.wav', 'click.ogg'] });
 export function loadAudio(state, eventCode, howlArgs) {
-    const id = startLoading(state, 0); // 0 size so 'load_progress' event can fire
     let src = howlArgs.src;
-    if (!Array.isArray(src)) {
+    if (!isArray(src)) {
         src = [src];
     }
     for (const path of src) {
         const extension = extname(path).substr(1);
         if (Howler.codecs(extension)) {
-            loadData(state, path, undefined)
-                .then((_data) => {
-                    const args = { ...howlArgs, src: howlArgs.src, format: [extension] };
-                    load(state, eventCode, args, id);
-                });
+            let { promise, loadingID } = loadData(state, path, undefined);
+
+            promise = promise.then(() => {
+                const args = { ...howlArgs, src: howlArgs.src, format: [extension] };
+                load(state, eventCode, args);
+            });
+
+            finalizeLoadingPromise(state, loadingID, promise);
             return;
         }
     }
-    errorLoading(state);
+    errorLoading(state, new Error(`Unsupported audio format: [${src}]`));
 }
 
 export function muted() {
